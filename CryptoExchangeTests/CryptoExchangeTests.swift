741e7521e72351d76622ae47fee04090b74ce305
Igor Custodio <igor.custodio@alayacare.com>
2025-05-16 23:05:26 -0300

    Initial Commit

---
+++ CryptoExchangeTests/CryptoExchangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoExchangeTests.swift
+//  CryptoExchangeTests
+//
+//  Created by Igor Custodio on 16/05/25.
+//
+
+import Testing
+@testable import CryptoExchange
+
+struct CryptoExchangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
