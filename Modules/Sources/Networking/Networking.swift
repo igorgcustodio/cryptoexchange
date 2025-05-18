eddef3d7b735005d6f589bdda54cc60c13b9da11
Igor Custodio <igor.custodio@alayacare.com>
2025-05-17 22:28:45 -0300

    core(networking): add networking layer

--- Modules/Sources/Networking/Networking.swift
+++ Modules/Sources/Networking/Networking.swift
@@ -1,1 +1,111 @@
-public enum Networking { }
+import Foundation
+
+public enum Networking {
+    public enum Method: String {
+        case GET, POST, PUT, DELETE
+    }
+
+    public protocol Route {
+        var path: String { get }
+        var method: Method { get }
+        var body: Encodable? { get }
+        var headers: [String: String] { get }
+        var queryItems: [URLQueryItem] { get }
+        var timeout: TimeInterval { get }
+        var cachePolicy: URLRequest.CachePolicy { get }
+        var usesAbsoluteURL: Bool { get }
+    }
+
+    public enum ResponseError: Error {
+        case noResponse
+        case unexpected
+        case decodingFailed(underlyingError: Error)
+        case clientError(code: Int)
+        case serverError(code: Int)
+        case unkonwn(description: String)
+
+        public var localizedDescription: String {
+            switch self {
+            case let .clientError(code):
+                return "Client error (\(code))"
+            case let .serverError(code):
+                return "Server error (\(code))"
+            case let .unkonwn(description):
+                return description
+            case .noResponse:
+                return "Failed to convert the URLResponse"
+            case .unexpected:
+                return "Unexpected error"
+            case .decodingFailed:
+                return "Failed to decode"
+            }
+        }
+    }
+
+    public protocol Resource: Decodable {
+        static func decode(from: Data) throws -> Self
+    }
+
+    public struct RequestBuilder {
+        let baseUrl: String
+        let route: Route
+
+        public enum RequestError: Error {
+            case badUrl
+            case encodingFailed
+            case invalidQueryItems
+        }
+
+        func buildRequest() throws -> URLRequest {
+            guard var url = URL(string: baseUrl + route.path)
+            else {
+                throw RequestError.badUrl
+            }
+
+            if !route.queryItems.isEmpty {
+                var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+                components?.queryItems = route.queryItems
+
+                if let componentUrl = components?.url {
+                    url = componentUrl
+                } else {
+                    throw RequestError.invalidQueryItems
+                }
+            }
+
+            var request = URLRequest(
+                url: url,
+                cachePolicy: route.cachePolicy,
+                timeoutInterval: route.timeout
+            )
+
+            request.httpMethod = route.method.rawValue
+
+            for header in route.headers {
+                request.setValue(header.value, forHTTPHeaderField: header.key)
+            }
+
+            if route.method != .GET, let body = route.body {
+                do {
+                    let data = try JSONEncoder().encode(body)
+                    request.httpBody = data
+                } catch {
+                    throw RequestError.encodingFailed
+                }
+            }
+
+            return request
+        }
+    }
+}
+
+extension Networking.Route {
+    public var timeout: TimeInterval { 60 }
+    public var cachePolicy: URLRequest.CachePolicy { .reloadIgnoringLocalCacheData }
+}
+
+extension Networking.Resource {
+    public static func decode(from data: Data) throws -> Self {
+        try JSONDecoder().decode(Self.self, from: data)
+    }
+}
---
+++ Modules/Sources/Networking/NetworkingService.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+public typealias NetworkingService = Networking.NetworkingService
+
+extension Networking {
+    public class NetworkingService<R: Route> {
+        let urlSession: URLSessionProtocol
+        let baseUrl: String
+
+        public init(urlSession: URLSessionProtocol = URLSession.shared, baseUrl: String = KeyConfig.baseURL) {
+            self.urlSession = urlSession
+            self.baseUrl = baseUrl
+        }
+
+        public func request<T: Resource>(_ route: R) async throws -> T {
+            let data: Data? = try await request(route)
+
+            guard let data else {
+                throw ResponseError.unexpected
+            }
+
+            do {
+                return try T.decode(from: data)
+            } catch let error as DecodingError {
+                throw ResponseError.decodingFailed(underlyingError: error)
+            } catch {
+                throw ResponseError.unexpected
+            }
+        }
+
+        public func request(_ route: R) async throws -> Data? {
+            let requestBuilder = RequestBuilder(baseUrl: route.usesAbsoluteURL ? "" : baseUrl, route: route)
+
+            let request = try requestBuilder.buildRequest()
+
+            let (data, response) = try await urlSession.data(for: request)
+
+            guard let response = response as? HTTPURLResponse else {
+                throw ResponseError.noResponse
+            }
+
+            switch response.statusCode {
+            case 200..<300:
+                return data
+            case 400..<500:
+                throw ResponseError.clientError(code: response.statusCode)
+            case 500..<600:
+                throw ResponseError.serverError(code: response.statusCode)
+            default:
+                throw ResponseError.unexpected
+            }
+
+        }
+    }
+}
